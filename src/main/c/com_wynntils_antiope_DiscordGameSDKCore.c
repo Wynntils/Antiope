f853758876165f57a53d3dfb7ab2f696b16e4ae7
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-06-23 20:32:51 +0200

    fix: Fix C core file not matching java file

--- src/main/c/com_wynntils_antiope_DiscordGameSDKCore.c
+++ src/main/c/com_wynntils_antiope_DiscordGameSDKCore.c
@@ -6,11 +6,11 @@
 
 #include <discord_game_sdk.h>
 
-#include "com_wynntils_antiope_Core.h"
+#include "com_wynntils_antiope_DiscordGameSDKCore.h"
 
 void* handle = NULL;
 
-JNIEXPORT void JNICALL Java_com_wynntils_antiope_Core_initDiscordNative(JNIEnv *env, jclass clazz, jstring path)
+JNIEXPORT void JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_initDiscordNative(JNIEnv *env, jclass clazz, jstring path)
 {
 #if defined(linux) || defined(__APPLE__)
 	if(handle)
@@ -37,7 +37,7 @@ JNIEXPORT void JNICALL Java_com_wynntils_antiope_Core_initDiscordNative(JNIEnv *
 #endif
 }
 
-JNIEXPORT jobject JNICALL Java_com_wynntils_antiope_Core_create(JNIEnv *env, jobject object, jlong param_pointer)
+JNIEXPORT jobject JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_create(JNIEnv *env, jobject object, jlong param_pointer)
 {
 	struct DiscordCreateParams *params = (struct DiscordCreateParams*) param_pointer;
 	
@@ -82,25 +82,25 @@ JNIEXPORT jobject JNICALL Java_com_wynntils_antiope_Core_create(JNIEnv *env, job
 	}
 }
 
-JNIEXPORT void JNICALL Java_com_wynntils_antiope_Core_destroy(JNIEnv *env, jobject object, jlong pointer)
+JNIEXPORT void JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_destroy(JNIEnv *env, jobject object, jlong pointer)
 {
 	struct IDiscordCore* core = (struct IDiscordCore*) pointer;
 	core->destroy(core);
 }
 
-JNIEXPORT jlong JNICALL Java_com_wynntils_antiope_Core_getActivityManager(JNIEnv *env, jobject object, jlong pointer)
+JNIEXPORT jlong JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_getActivityManager(JNIEnv *env, jobject object, jlong pointer)
 {
 	struct IDiscordCore* core = (struct IDiscordCore*) pointer;
 	return (uint64_t) core->get_activity_manager(core);
 }
 
-JNIEXPORT jlong JNICALL Java_com_wynntils_antiope_Core_getOverlayManager(JNIEnv *env, jobject object, jlong pointer)
+JNIEXPORT jlong JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_getOverlayManager(JNIEnv *env, jobject object, jlong pointer)
 {
 	struct IDiscordCore* core = (struct IDiscordCore*) pointer;
 	return (uint64_t) core->get_overlay_manager(core);
 }
 
-JNIEXPORT void JNICALL Java_com_wynntils_antiope_Core_runCallbacks(JNIEnv *env, jobject object, jlong pointer)
+JNIEXPORT void JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_runCallbacks(JNIEnv *env, jobject object, jlong pointer)
 {
 	struct IDiscordCore* core = (struct IDiscordCore*) pointer;
 	core->run_callbacks(core);
@@ -137,7 +137,7 @@ void log_hook(void* data, enum EDiscordLogLevel level, const char* message)
 	(*(hook_data->jvm))->DetachCurrentThread(hook_data->jvm);
 }
 
-JNIEXPORT void JNICALL Java_com_wynntils_antiope_Core_setLogHook(JNIEnv *env, jobject object, jlong pointer, jint min_level, jobject hook)
+JNIEXPORT void JNICALL Java_com_wynntils_antiope_DiscordGameSDKCore_setLogHook(JNIEnv *env, jobject object, jlong pointer, jint min_level, jobject hook)
 {	
 	struct IDiscordCore* core = (struct IDiscordCore*) pointer;
 	
