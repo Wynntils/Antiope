673a1b8f51580c833094d5016787f334b6d878f9
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-07-10 23:54:32 -0700

    more possible macos fix

--- toolchains/macos-amd64.cmake
+++ toolchains/macos-amd64.cmake
@@ -6,4 +6,8 @@ set(CMAKE_C_FLAGS -fuse-ld=/home/ryan/osxcross/target/bin/x86_64-apple-darwin20.
 set(CMAKE_C_COMPILER o64-clang)
 
 set(CMAKE_SHARED_LIBRARY_SUFFIX_C ".dylib")
-set(JAVA_INCLUDE_PATH /usr/lib/jvm/macos-aarch64-jdk-11.0.19/Contents/Home/include)
+set(JAVA_INCLUDE_PATH /usr/lib/jvm/macos-x64-jdk-11.0.19/Contents/Home/include)
+set(JAVA_INCLUDE_PATH2 /usr/lib/jvm/macos-x64-jdk-11.0.19/Contents/Home/include/darwin)
+set(JAVA_AWT_INCLUDE_PATH NotNeeded)
+set(JAVA_AWT_LIBRARY NotNeeded)
+set(JAVA_JVM_LIBRARY NotNeeded)
