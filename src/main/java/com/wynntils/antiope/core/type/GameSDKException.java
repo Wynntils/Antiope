cd4d546670ad031871b7d763dba633e1021b53d9
Ryan <57310593+DonkeyBlaster@users.noreply.github.com>
2023-07-12 23:01:09 -0700

    fix: GameSDKException did not assign result variable (#15)

--- src/main/java/com/wynntils/antiope/core/type/GameSDKException.java
+++ src/main/java/com/wynntils/antiope/core/type/GameSDKException.java
@@ -4,10 +4,11 @@ package com.wynntils.antiope.core.type;
  * Exception which is thrown when a {@link Result} that is not {@link Result#OK} occurs.
  */
 public class GameSDKException extends RuntimeException {
-    private Result result;
+    private final Result result;
 
     public GameSDKException(Result result) {
         super("Game SDK operation failed: " + result);
+        this.result = result;
     }
 
     /**
